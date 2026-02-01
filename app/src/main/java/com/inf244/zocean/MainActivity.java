514103371d01f687ee7d7f111602684dcf799daa
yan <yanyanportez3@gmail.com>
2026-02-01 19:27:05 +0800

    updated v2

--- app/src/main/java/com/inf244/zocean/MainActivity.java
+++ app/src/main/java/com/inf244/zocean/MainActivity.java
@@ -3,6 +3,7 @@ package com.inf244.zocean;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -45,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        Log.d("MainActivity", "onResume() called - updating sign out button");
         updateSignOutButtonVisibility();
     }
 
@@ -52,15 +54,26 @@ public class MainActivity extends AppCompatActivity {
         boolean isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false);
         String userType = sharedPreferences.getString("userType", "");
 
+        Log.d("MainActivity", "=== CHECKING SIGN OUT VISIBILITY ===");
+        Log.d("MainActivity", "isLoggedIn = " + isLoggedIn);
+        Log.d("MainActivity", "userType = " + userType);
+        Log.d("MainActivity", "Should show button? " + (isLoggedIn && userType.equals("guest")));
+
         if (isLoggedIn && userType.equals("guest")) {
             signOutButton.setVisibility(View.VISIBLE);
+            Log.d("MainActivity", "Sign Out button SET TO VISIBLE");
         } else {
             signOutButton.setVisibility(View.GONE);
+            Log.d("MainActivity", "Sign Out button SET TO GONE");
         }
     }
 
     private void handleReservation(String roomType, int roomPrice) {
         boolean isLoggedIn = sharedPreferences.getBoolean("isLoggedIn", false);
+
+        Log.d("MainActivity", "=== RESERVE CLICKED ===");
+        Log.d("MainActivity", "isLoggedIn = " + isLoggedIn);
+        Log.d("MainActivity", "Going to: " + (isLoggedIn ? "DateSelection" : "LoginActivity"));
 
         Intent intent;
         if (isLoggedIn) {
@@ -73,14 +86,17 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("ROOM_TYPE", roomType);
         intent.putExtra("ROOM_PRICE", roomPrice);
         startActivity(intent);
+
     }
 
     private void signOut() {
+        Log.d("MainActivity", "=== SIGN OUT CLICKED ===");
+
         sharedPreferences.edit().clear().apply();
 
         Toast.makeText(MainActivity.this, "Signed out successfully", Toast.LENGTH_SHORT).show();
 
-        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(intent);
         finish();
